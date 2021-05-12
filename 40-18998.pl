353a0d67a2d8f09be1b92131c625f83b1547cd63
MaherMohammed <47296887+MaherMohammed@users.noreply.github.com>
2021-05-12 07:20:20 +0200

    Add files via upload

---
+++ 40-18998.pl
@@ -0,0 +1,112 @@
+sentence(s(NP,VP)) --> noun_phrase(NP), verb_phrase(VP).
+noun_phrase(np(D,N)) --> det(D), noun(N).
+verb_phrase(vp(V,NP)) --> verb(V), noun_phrase(NP).
+
+det(d(the)) --> [the].
+det(d(a)) --> [a].
+det(d(some)) --> [some].
+det(d(many)) --> [many].
+det(d(every)) --> [every].
+
+noun(n(boy)) --> [boy].
+noun(n(box)) --> [box].
+noun(n(room)) --> [room].
+noun(n(school)) --> [school].
+noun(n(woman)) --> [woman].
+noun(n(man)) --> [man].
+noun(n(envelope)) --> [envelope].
+noun(n(shed)) --> [shed].
+noun(n(building)) --> [building].
+noun(n(tree)) --> [tree].
+noun(n(girl)) --> [girl].
+noun(n(students)) --> [students].
+noun(n(professors)) --> [professors].
+noun(n(lecturers)) --> [lecturers].
+noun(n(scientists)) --> [scientists].
+noun(n(researchers)) --> [researchers].
+noun(n(father)) --> [father].
+noun(n(mother)) --> [mother].
+noun(n(cat)) --> [cat].
+noun(n(fish)) --> [fish].
+
+
+
+verb(v(eats)) --> [eats].
+verb(v(push)) --> [push].
+verb(v(pushed)) --> [pushed].
+verb(v(store)) --> [store].
+verb(v(stored)) --> [stored].
+verb(v(give)) --> [give].
+verb(v(belongs)) --> [belongs].
+verb(v(climb)) --> [climb].
+verb(v(climbed)) --> [climbed].
+verb(v(watched)) --> [watch].
+verb(v(do)) --> [do].
+verb(v(did)) --> [did].
+verb(v(admire)) --> [admire].
+verb(v(admired)) --> [admired].
+verb(v(run)) --> [run].
+verb(v(ran)) --> [ran].
+verb(v(speak)) --> [speak].
+verb(v(tells)) --> [tells].
+verb(v(walks)) --> [walks].
+verb(v(smells)) --> [smells].
+verb(v(gets)) --> [gets].
+verb(v(plays)) --> [plays].
+
+adjective(adj(young)) --> [young].
+adjective(adj(empty)) --> [empty].
+adjective(adj(large)) --> [large].
+adjective(adj(big)) --> [big].
+adjective(adj(old)) --> [old].
+adjective(adj(poor)) --> [poor].
+adjective(adj(white)) --> [white].
+adjective(adj(briliant)) --> [briliant].
+adjective(adj(talented)) --> [talented].
+adjective(adj(bright)) --> [bright].
+adjective(adj(beautiful)) --> [beautiful].
+adjective(adj(small)) --> [small].
+adjective(adj(good)) --> [good].
+adjective(adj(handsome)) --> [handsome].
+adjective(adj(high)) --> [high].
+adjective(adj(gorgeous)) --> [gorgeous].
+adjective(adj(bad)) --> [bad].
+adjective(adj(busy)) --> [busy].
+adjective(adj(calm)) --> [calm].
+adjective(adj(afraid)) --> [afraid].
+
+
+adverb(adv(quickly)) --> [quickly].
+adverb(adv(early)) --> [early].
+adverb(adv(here)) --> [here].
+adverb(adv(first)) --> [first].
+adverb(adv(last)) --> [last].
+adverb(adv(too)) --> [too].
+adverb(adv(very)) --> [very].
+adverb(adv(simply)) --> [simply].
+adverb(adv(literally)) --> [literally].
+adverb(adv(really)) --> [really].
+adverb(adv(absolutely)) --> [absolutely].
+adverb(adv(enough)) --> [enough].
+adverb(adv(now)) --> [now].
+adverb(adv(quite)) --> [quite].
+adverb(adv(today)) --> [today].
+adverb(adv(outside)) --> [outside].
+adverb(adv(there)) --> [there].
+adverb(adv(cheerfully)) --> [cheerfully].
+adverb(adv(somewhere)) --> [somewhere].
+adverb(adv(totally)) --> [totally].
+
+preposition(pre(after)) --> [after].
+preposition(pre(in)) --> [in].
+preposition(pre(behind)) --> [behind].
+preposition(pre(on)) --> [on].
+preposition(pre(under)) --> [under].
+preposition(pre(over)) --> [over].
+preposition(pre(at)) --> [at].
+preposition(pre(to)) --> [to].
+preposition(pre(by)) --> [by].
+preposition(pre(from)) --> [from].
+
+interrogative(intr(who)) --> [who].
+interrogative(intr(what)) --> [what].
